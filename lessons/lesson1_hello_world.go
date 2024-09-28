503d8f8bffe4193ff474668d09d941befb806048
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-09-28 12:00:26 +0700

    Add hello world lession

---
+++ lessons/lesson1_hello_world.go
@@ -0,0 +1,5 @@
+package lesson1_hello_world
+
+func HelloWorld() string {
+	return "Hello, World"
+}
---
+++ lessons/lesson1_hello_world_test.go
@@ -0,0 +1,17 @@
+package lesson1_hello_world
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHelloWorld(t *testing.T) {
+
+	t.Run("should return hello world message", func(t *testing.T) {
+		expected := "Hello, World"
+		actual := HelloWorld()
+
+		assert.Equal(t, expected, actual)
+	})
+}
