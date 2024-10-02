acadacb5a069cdc9daa3a42bfac47db8e44e5eb8
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-10-02 13:31:16 +0700

    Add add example in map lesson

--- lessons/09_map/map.go
+++ lessons/09_map/map.go
@@ -4,12 +4,24 @@ import "errors"
 
 type Dictionary map[string]string
 
-func (dictionary Dictionary) Search(key string) (string, error) {
-	value, ok := dictionary[key]
+func (dictionary Dictionary) Search(extension string) (string, error) {
+	language, existed := dictionary[extension]
 
-	if !ok {
-		return value, errors.New("programming language does not exist")
+	if !existed {
+		return language, errors.New("programming language does not exist")
 	}
 
-	return value, nil
+	return language, nil
+}
+
+func (dictionary Dictionary) Add(extension, language string) error {
+	_, existed := dictionary[extension]
+
+	if existed {
+		return errors.New("programming language already exists")
+	}
+
+	dictionary[extension] = language
+
+	return nil
 }
--- lessons/09_map/map_test.go
+++ lessons/09_map/map_test.go
@@ -32,3 +32,26 @@ func TestSearch(t *testing.T) {
 		assert.EqualError(t, err, "programming language does not exist")
 	})
 }
+
+func TestAdd(t *testing.T) {
+	t.Run("should add new programming language if given file extension does NOT exist", func(t *testing.T) {
+		dictionary := Dictionary{}
+
+		err := dictionary.Add("go", "Go")
+		expected := "Go"
+		actual := dictionary["go"]
+
+		assert.Equal(t, expected, actual)
+		assert.Nil(t, err)
+	})
+
+	t.Run("should return error if given file extension already exists", func(t *testing.T) {
+		dictionary := Dictionary{
+			"go": "Go",
+		}
+
+		err := dictionary.Add("go", "Go")
+
+		assert.EqualError(t, err, "programming language already exists")
+	})
+}
