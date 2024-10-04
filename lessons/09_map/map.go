51f72128a868dd8787ae247bc834ff14763fd689
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-10-04 16:10:41 +0700

    Refactor errors to separated variables

--- lessons/09_map/map.go
+++ lessons/09_map/map.go
@@ -4,11 +4,16 @@ import "errors"
 
 type Dictionary map[string]string
 
+var (
+	errItemNotExistErr    = errors.New("programming language does not exist")
+	erroItemAlreadyExists = errors.New("programming language already exists")
+)
+
 func (dictionary Dictionary) Search(extension string) (string, error) {
 	language, existed := dictionary[extension]
 
 	if !existed {
-		return language, errors.New("programming language does not exist")
+		return language, errItemNotExistErr
 	}
 
 	return language, nil
@@ -18,7 +23,7 @@ func (dictionary Dictionary) Add(extension, language string) error {
 	_, existed := dictionary[extension]
 
 	if existed {
-		return errors.New("programming language already exists")
+		return erroItemAlreadyExists
 	}
 
 	dictionary[extension] = language
